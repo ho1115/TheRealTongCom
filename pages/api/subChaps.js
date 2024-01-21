ddd5ce9367b3d62ca61159333f87f326d286527d
Lab303Slave <ck1130522@gmail.com>
2024-01-21 16:45:35 +0800

    DATATOOLARGE

--- pages/api/subChaps.js
+++ pages/api/subChaps.js
@@ -1,13 +1,29 @@
 const ms = require('mysql2') 
-const conn = ms.createConnection({
-    host: process.env.HOST,
-    database: process.env.DATABASE,
-    user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
-    port: process.env.SQLPORT
-})
+
+async function idChapterList(conn) {
+    var list = {}
+    var sql = 'SELECT DBID, book_name, volumn, word_count, all_lap_length, cut_lap_length FROM official_history';
+    const qRes = await conn.promise().query(sql)
+    for (let i = 0; i < qRes[0].length; i++) {
+        list[qRes[0][i]['DBID']] = {
+            'bname' : qRes[0][i]['book_name'],
+            'volu' : qRes[0][i]['volumn'],
+            'len' : qRes[0][i]['word_count'],
+            'all_lap_length' : qRes[0][i]['all_lap_length'],
+            'cut_lap_length' : qRes[0][i]['cut_lap_length'],
+        }
+    }                       
+    return list
+}
 
 export default async function subChaps(bookName, chapName, version) {
+    const conn = ms.createConnection({
+        host: process.env.HOST,
+        database: process.env.DATABASE,
+        user: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASSWORD,
+        port: process.env.SQLPORT
+    })
     
     var cnt = version === 'all' ? 'all_lap_length' : 'cut_lap_length';
     var relationTable = version === 'all' ? 'old_relations' : 'tongzhi_official_history_relations';
@@ -15,10 +31,10 @@ export default async function subChaps(bookName, chapName, version) {
     const tzList = ['本紀', '后妃', '世家', '列傳', '外戚傳', '忠義傳', '孝友傳', '獨行傳', '循吏傳', '酷吏傳', '儒林傳', '文苑傳',
                     '隠逸傳', '宦者傳', '游俠傳', '藝術傳', '佞幸傳', '列女傳', '載記','四夷']
 
-    if (tzList.includes(chapName)) {
-        var idToChap = await tool.idChapterList()
+    if (tzList.includes(chapName.split(' ')[0])) {
+        var idToChap = await idChapterList(conn)
         var sql = 'SELECT chapter_number, DBID, dynasty, name, word_count, ' + cnt + ' FROM tongzhi WHERE chapter = ?'
-        var qRes = await conn.promise().query(sql, [chapName])
+        var qRes = await conn.promise().query(sql, [chapName.split(' ')[0]]);
         for (let i = 0; i < qRes[0].length; i++) {
             if (result[qRes[0][i]['chapter_number']] == null) {result[qRes[0][i]['chapter_number']] = {}}
             var tongid = qRes[0][i]['DBID']
@@ -26,7 +42,7 @@ export default async function subChaps(bookName, chapName, version) {
             var mRes = await conn.promise().query(matchsql, [tongid])
             var matchChapJs = {}
             for (let j = 0; j < mRes[0].length; j++) {
-                var concatName = idToChap[str(mRes[0][j]['official_history_ID'])]['bname'] + ' ' + idToChap[str(mRes[0][j]['official_history_ID'])]['volu']
+                var concatName = idToChap[mRes[0][j]['official_history_ID']]['bname'] + ' ' + idToChap[mRes[0][j]['official_history_ID']]['volu']
                 matchChapJs[concatName] = [mRes[0][j]['match_word_count'], mRes[0][j]['official_history_ID']]
             }
             result[qRes[0][i]['chapter_number']][qRes[0][i]['name']] = {
@@ -63,7 +79,6 @@ export default async function subChaps(bookName, chapName, version) {
         }
         
     }
-    console.log(result)
-    conn.end()
+    conn.end();
     return result
 }
--- pages/filtered/compRes/[books]/[chaps]/index.js
+++ pages/filtered/compRes/[books]/[chaps]/index.js
@@ -1,17 +1,18 @@
 import Topnav from "@/pages/pageComps/topnav"
 import Hislist from "@/pages/pageComps/hislist"
-import subChaps from "@/pages/api/subChaps"
-import { useRouter } from "next/router";
+
+
 import pgData from "@/pages/pgData.json"
 
 
 
-export async function getServerSideProps(context) {
-  const bookName = pgData.engToCh[context.params.books]
-  const chapName = pgData.booksNchaps[bookName][context.params.chaps - 1]
-  const posts = await subChaps(bookName, chapName, 'cut')
-  return { props: { posts } }
-}
+// export async function getServerSideProps(context) {
+  
+  // const bookName = pgData.engToCh[context.params.books]
+  // const chapName = pgData.booksNchaps[bookName][context.params.chaps - 1]
+  // const posts = await subChaps(bookName, chapName, 'cut')
+  // return { props: { posts } }
+// }
 
 export default function Home({ posts }) {
   
@@ -19,7 +20,7 @@ export default function Home({ posts }) {
     <div className = "inline-flex flex-wrap bg-black text-white">
       <Topnav />
       <Hislist />
-      <div className = "inline-flex pl-1/6 bg-orange"><h1>{posts}</h1></div>
+      <div className = "inline-flex pl-1/6 bg-orange"><h1>fuck</h1></div>
     </div>
   )
 
